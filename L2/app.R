6c4ade816122ae8456fa16d8176d1f214f21ff12
chitra.m.saraswati@gmail.com <chitra.m.saraswati@gmail.com>
2024-05-03 16:29:09 +0800

    Lesson 1

---
+++ L2/app.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(bslib)
+
+# Define UI ----
+ui <- page_sidebar(
+  
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
